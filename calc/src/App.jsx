c750eacd40fe118b26d9466b52f1e4db7d791742
DamianToromanovic <d.toromanovic@web.de>
2025-03-24 16:13:33 +0100

    ss

--- calc/src/App.jsx
+++ calc/src/App.jsx
@@ -5,9 +5,11 @@ import Operators from "./components/Operators";
 
 function App() {
   return (
-    <main>
-      <Numbers />
-      <Operators />
+    <main className="bg-gray-100 text-gray-900 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg grid grid-cols-3 gap-4">
+        <Numbers />
+        <Operators />
+      </div>
     </main>
   );
 }
