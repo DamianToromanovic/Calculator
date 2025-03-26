abd8c41cf06fcd2b0752c159ac364ff6d3af0121
DamianToromanovic <d.toromanovic@web.de>
2025-03-26 14:21:46 +0100

    merged on main

--- calc/src/components/Display.jsx
+++ calc/src/components/Display.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Display({ input }) {
   return (
-    <div className="col-span-4 row-end-2 bg-gray-200 p-4 rounded text-right text-2xl">
+    <div className="col-span-4 row-end-2 bg-gray-200 p-2 rounded text-xl text-slate-900 flex items-center justify-end">
       {input}
     </div>
   );
